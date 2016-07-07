96aff2c1953502a84f1796440e833b75962d9ce8
Roy N Emanuel II <roy.emanuel@gmail.com>
2016-07-07 05:57:14 -0400

    Start of a plotterfile to separate the functions from the plots.

---
+++ plotBuild.R
@@ -0,0 +1,9 @@
+######################################################################
+## Building the Plots
+######################################################################
+
+## Plot of Resilience (Y axis) against a constant Need from 0 to 1.2
+
+## This will be somewhat hard coded. Every function should reference
+## metrics.R. Using ESS, use the C-c C-f to fire off the function and
+## C-ENT rather than the entire file. 
