ece5381a4bbcda417962abd2c6bc9c403b64dd80
Roy N Emanuel II <roy.emanuel@gmail.com>
2016-09-15 19:01:17 -0400

    Added inheritance of Part to Avionics, Airframe, and Propulsion. Build a single aircraft for an example.

--- fleetResilience.py
+++ fleetResilience.py
@@ -13,24 +13,42 @@ RS = random.seed(42)
 
 
 # Build an aircraft part
+# an aircraft part needs to work, fail, and be part of an aircraft
 class Part(object):
-    def __init__(self): class Airframe(Part):
-    def __init__(self, env, BuNo):
+    def __init__(self, env, ID):
+        self.ID = ID
+        self.age = 0
+        self.fail = 0
+        self.status = "UP"
+
+
+# The parts have different types of parts. I think this is the way
+# to nest or do the inheritance part of OOP.
+# Airframes are the source of the BuNo
+class Airframe(Part):
+    def __init__(self, env, ID):
         self.env = env
-        self.BuNo = BuNo
+        super().__init__(env, ID)
         self.status = 1
+
+
+# Avionics and Propusion behave the same. They will just draw from
+# different distributions and be party to different updates / Tech
+# refreshes.
 
 
 class Avionics(Part):
-    def __init__(self, env):
+    def __init__(self, env, ID):
         self.env = env
         self.status = 1
+        super().__init__(env, ID)
 
 
 class Propulsion(Part):
-    def __init__(self, env):
+    def __init__(self, env, ID):
         self.env = env
         self.status = 1
+        super().__init__(env, ID)
 
 
 class Aircraft(object):
@@ -39,8 +57,19 @@ class Aircraft(object):
         self.af = af
         self.av = av
         self.puls = puls
-        self.BuNo = af.BuNo
-        self.fly_proc = env.process(self.fly(env))
+        self.BuNo = self.af.ID
 
-    def fly(self, env):
+    #def start(self, env):
+        # if (self.af.age() >)
+    # def fly(self, env):
         
+
+
+env = simpy.Environment()
+
+
+# Create an airplane to 
+av1 = Avionics(env, "av1")
+af1 = Airframe(env, "af1")
+puls1 = Propulsion(env, "puls1")
+ac1 = Aircraft(env, af1, av1, puls1)
