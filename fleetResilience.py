48ee9a66c8361edebe4745532bb1d830d2b305f4
Roy N Emanuel II <roy.emanuel@gmail.com>
2017-07-10 16:36:57 -0400

    BROKEN
    Started to make some changes to better track the information on the pilots. I believe it

--- fleetResilience.py
+++ fleetResilience.py
@@ -215,6 +215,7 @@ class Aircrew(object):
         self.flightDF = self.flightDF.append({"Stud ID": self.ID,
                                               "Flight Time": fltTime,
                                               "Aircraft": ac,
+                                              "Result":
                                               "Takeoff Time": env.now},
                                              ignore_index=True)
 
