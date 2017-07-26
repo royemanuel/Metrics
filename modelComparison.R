115fec4cf8a1f9135c318b2d5697f2bd4244cbe2
Roy Emanuel <roy.emanuel@gmail.com>
2017-07-26 15:12:50 -0400

    Adding a read.csv for the data so we can do separate analysis that shows the numbers and not just plots.

---
+++ modelComparison.R
@@ -0,0 +1,13 @@
+## The purpose of this file is to draw out the values of the
+
+steppedRecoveryTimeHorizonDataPull <- read.csv("steppedRecoveryTimeHorizonData.csv")
+need0to1SteppedRecoveryDataPull <- read.csv("need0to1SteppedRecoveryData.csv")
+sigma0to1SteppedRecoveryDataPull <- read.csv("sigma0to1SteppedRecoveryData.csv")
+failLevel0to1DataPull <- read.csv("failLevel0to1Data.csv")
+recoveryLevel0to1DataPull <- read.csv("recoveryLevel0to1Data.csv")
+recoveryTime21to60DataPull <- read.csv("recoveryTime21to60Data.csv")
+linearRecoveryTimeHorizonDataPull <- read.csv("linearRecoveryTimeHorizonData.csv")
+need0to1LinearRecoveryDataPull <- read.csv("need0to1LinearRecoveryData.csv")
+sigma0to1LinearRecoveryDataPull <- read.csv("sigma0to1LinearRecoveryData.csv")
+
+
