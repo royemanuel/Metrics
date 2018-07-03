8c35ce1b07fbcb2a03f92f6a730fae08b2966c15
Roy N Emanuel II <roy.emanuel@gmail.com>
2018-07-03 11:50:42 -0400

    adding a script to do some analysis on time to graduate.

---
+++ fltCalcTTG.R
@@ -0,0 +1,5 @@
+######################################################################
+## This is a lone script to calculate some time to graduate numbers
+## so I can set a reasonable threshold
+######################################################################
+
