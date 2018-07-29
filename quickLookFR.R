c9fcc3c04ce4100c8c39ab61b0bcffcad05378ec
Roy N Emanuel II <roy.emanuel@gmail.com>
2018-07-29 19:42:47 -0400

    Building for the paper.

--- quickLookFR.R
+++ quickLookFR.R
@@ -1,12 +1,30 @@
 library("tidyverse")
 library("readxl")
 
-skedTracker <- read_csv("skedTrackerExp1Run1.csv")
+skedTracker1 <- read_csv("skedTrackerExp1Run0RS605702648.csv") %>%
+    mutate(Exp = "1")
+skedTracker2 <- read_csv("skedTrackerExp2Run0RS605702648.csv") %>%
+    mutate(Exp = "2")
+skedTracker3 <- read_csv("skedTrackerExp3Run0RS605702648.csv") %>%
+    mutate(Exp = "3")
+skedTracker4 <- read_csv("skedTrackerExp4Run0RS605702648.csv") %>%
+    mutate(Exp = "4")
+skedTracker5 <- read_csv("skedTrackerExp5Run0RS605702648.csv") %>%
+    mutate(Exp = "5")
+skedTracker6 <- read_csv("skedTrackerExp6Run0RS605702648.csv") %>%
+    mutate(Exp = "6")
+
+skedTracker <- bind_rows(skedTracker1,
+                         skedTracker2,
+                         skedTracker3,
+                         skedTracker4,
+                         skedTracker5,
+                         skedTracker6)
 
 skedTrackerPlotter <-
     skedTracker %>%
     select(-X1, -Day, -instructors) %>%
-    gather(Category, Count, -Time) %>%
+    gather(Category, Count, -Time, -Exp) %>%
     mutate(Time = Time / (24*365))
 
 studPlot <-
@@ -14,20 +32,28 @@ studPlot <-
            aes(Time, Count,
                group = Category,
                color = Category)) +
-    geom_line()
+    geom_line() +
+    facet_wrap(~ Exp, ncol = 1)
 
 skedTrackerNG <-
     skedTrackerPlotter %>%
     filter(Category != "graduates",
            Category != "attrites",
-           Category != "students")
+           Category != "students",
+           Category != "boneYard") %>%
+    mutate(Percentage = Count / 50) %>%
+    select( -Count)
 
 studPlotNoGrads <-
     ggplot(skedTrackerNG,
-           aes(Time, Count,
+           aes(Time, Percentage,
                group = Category,
-               color = Category)) +
-    geom_line()
+               linetype = Category)) +
+    geom_line() +
+    facet_wrap(~ Exp, ncol = 1) +
+    geom_hline(yintercept = 0.85,
+               colour = "grey70") +
+    theme_bw()
 
 studPlotNoGrads
 
